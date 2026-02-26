e163ac3d5817786773c093def35abec7cde2f5ea
aarondud <aarondudley00@gmail.com>
2026-02-26 18:06:23 +0800

    refactor: add responsive zoom (90% mobile, 80% desktop)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,18 @@ import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
 
+const applyZoom = () => {
+  const width = window.innerWidth;
+  if (width >= 768) {
+    document.body.style.zoom = "80%";
+  } else {
+    document.body.style.zoom = "90%";
+  }
+};
+
+applyZoom();
+window.addEventListener("resize", applyZoom);
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
